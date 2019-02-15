8dcfda0360fcb5199e15b05ce28b4fbb82aa20f0
TimHenkins <43077776+TimHenkins@users.noreply.github.com>
2019-02-14 19:33:40 -0800

    Update DIOrobot.py

--- DIOrobot.py
+++ DIOrobot.py
@@ -2,110 +2,105 @@ import wpilib
 
 from wpilib.drive import MecanumDrive
 
-
+HallEffect=0
 
 
 
 class MyRobot(wpilib.TimedRobot):
 
-    # Channels on the roboRIO that the motor controllers are plugged in to
+	# Channels on the roboRIO that the motor controllers are plugged in to
 
-    frontLeftChannel = 2
+	frontLeftChannel = 2
 
-    rearLeftChannel = 3
+	rearLeftChannel = 3
 
-    frontRightChannel = 1
+	frontRightChannel = 1
 
-    rearRightChannel = 0
-
-
-
-    # The channel on the driver station that the joystick is connected to
-
-    joystickChannel = 0
+	rearRightChannel = 0
 
 
 
-    def robotInit(self):
+	# The channel on the driver station that the joystick is connected to
 
-        """Robot initialization function"""
-
-        self.frontLeftMotor = wpilib.Talon(self.frontLeftChannel)
-
-        self.rearLeftMotor = wpilib.Talon(self.rearLeftChannel)
-
-        self.frontRightMotor = wpilib.Talon(self.frontRightChannel)
-
-        self.rearRightMotor = wpilib.Talon(self.rearRightChannel)
+	joystickChannel = 0
 
 
 
-        # invert the left side motors
+	def robotInit(self):
 
-        self.frontLeftMotor.setInverted(True)
+		"""Robot initialization function"""
 
+		self.frontLeftMotor = wpilib.Talon(self.frontLeftChannel)
 
+		self.rearLeftMotor = wpilib.Talon(self.rearLeftChannel)
 
-        # you may need to change or remove this to match your robot
+		self.frontRightMotor = wpilib.Talon(self.frontRightChannel)
 
-        self.rearLeftMotor.setInverted(True)
-
-
-
-        self.drive = MecanumDrive(
-
-            self.frontLeftMotor,
-
-            self.rearLeftMotor,
-
-            self.frontRightMotor,
-
-            self.rearRightMotor,
-
-        )
+		self.rearRightMotor = wpilib.Talon(self.rearRightChannel)
 
 
 
-        self.drive.setExpiration(0.1)
+		# invert the left side motors
+
+		self.frontLeftMotor.setInverted(True)
 
 
 
-        self.stick = wpilib.Joystick(self.joystickChannel)
+		# you may need to change or remove this to match your robot
 
-
-    def autonomousInit(self):
-        LS=wpilib.DigitalInput(9)
-        if LS.get()==1:
-            print("Found it")
-
-    def operatorControl(self):
-
-        """Runs the motors with Mecanum drive."""
+		self.rearLeftMotor.setInverted(True)
 
 
 
-        self.drive.setSafetyEnabled(True)
+		self.drive = MecanumDrive(
 
-        while self.isOperatorControl() and self.isEnabled():
+			self.frontLeftMotor,
 
-            # Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+			self.rearLeftMotor,
 
-            # This sample does not use field-oriented drive, so the gyro input is set to zero.
+			self.frontRightMotor,
 
-            self.drive.driveCartesian(
+			self.rearRightMotor,
 
-                self.stick.getX(), self.stick.getY(), self.stick.getZ(), 0
-
-            )
+		)
 
 
 
-            wpilib.Timer.delay(0.005)  # wait 5ms to avoid hogging CPU cycles
+		self.drive.setExpiration(0.1)
+
+
+
+		self.stick = wpilib.Joystick(self.joystickChannel)
+
+	def teleopInit(self):
+		global HallEffect
+		HallEffect=wpilib.DigitalInput(9)
+		
+	def teleopPeriodic(self):
+		global HallEffect
+		if HallEffect.get() ==True:
+			print("Found it")
+
+	def operatorControl(self):
+
+		"""Runs the motors with Mecanum drive."""
+		self.drive.setSafetyEnabled(True)
+
+		while self.isOperatorControl() and self.isEnabled():
+
+			# Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+
+			# This sample does not use field-oriented drive, so the gyro input is set to zero.
+
+			self.drive.driveCartesian(
+
+				self.stick.getX(), self.stick.getY(), self.stick.getZ(), 0
+
+			)
 
 
 
 
 
 if __name__ == "__main__":
-
-    wpilib.run(MyRobot)
+	wpilib.run(MyRobot)
